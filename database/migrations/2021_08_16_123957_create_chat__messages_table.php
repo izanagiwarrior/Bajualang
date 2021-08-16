f10d614413854ba4eee45ad1bcddb25e1fde0ba8
M. Faiz Triputra <m.faiztpaduhai@gmail.com>
2021-08-16 19:45:41 +0700

    chat and likes

---
+++ database/migrations/2021_08_16_123957_create_chat__messages_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChatMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chat__messages', function (Blueprint $table) {
+            $table->id();
+            $table->string('id_chat');
+            $table->text('chat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chat__messages');
+    }
+}
